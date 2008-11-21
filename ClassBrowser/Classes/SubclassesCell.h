6a39cede92e541a83192ad4a53ee719219d6371a
野村 憲男 <norio.nomura@gmail.com>
2008-11-21 21:13:41 +0900

    remove copyright sentence

--- ClassBrowser/Classes/SubclassesCell.h
+++ ClassBrowser/Classes/SubclassesCell.h
@@ -2,9 +2,6 @@
 //  SubclassesCell.h
 //  ClassBrowser
 //
-//  Created by 野村 憲男 on 08/11/18.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
--- ClassBrowser/Classes/SubclassesCell.m
+++ ClassBrowser/Classes/SubclassesCell.m
@@ -2,9 +2,6 @@
 //  SubclassesCell.m
 //  ClassBrowser
 //
-//  Created by 野村 憲男 on 08/11/18.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
-//
 
 #import "SubclassesCell.h"
 
