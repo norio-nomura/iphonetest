7ce024d828d779f4aec3630e8305d57a674f12e1
野村 憲男 <norio.nomura@gmail.com>
2008-11-23 17:52:58 +0900

    fix leak, improve search performance

--- ClassBrowser/Classes/ClassSearchViewController.h
+++ ClassBrowser/Classes/ClassSearchViewController.h
@@ -5,21 +5,23 @@
 
 #import <UIKit/UIKit.h>
 
-@class IndexedDataSource;
-
-@interface ClassSearchViewController : UIViewController<UITableViewDelegate,UISearchBarDelegate,UITabBarDelegate> {
+@interface ClassSearchViewController : UIViewController<UITableViewDelegate,UITabBarDelegate,UISearchBarDelegate> {
 	UITableView *tableView;
 	UISegmentedControl *segmentedControl;
+	UISearchBar *searchBar;
 	UITabBar *tabBar;
-	NSMutableArray *dataSourcesArray_;
-	NSMutableArray *initialDataSourcesArray_;
+	@private
+	NSMutableArray *dataSourcesArray;
+	NSMutableArray *initialDataSourcesArray;
+	NSString *previousSearchText;
 }
 
 @property (nonatomic,retain) IBOutlet UITableView *tableView;
 @property (nonatomic,retain) IBOutlet UISegmentedControl *segmentedControl;
+@property (nonatomic,retain) IBOutlet UISearchBar *searchBar;
 @property (nonatomic,retain) IBOutlet UITabBar *tabBar;
 @property (nonatomic,retain) NSMutableArray *dataSourcesArray;
 @property (nonatomic,retain) NSMutableArray *initialDataSourcesArray;
-
+@property (nonatomic,copy) NSString *previousSearchText;
 
 @end
--- ClassBrowser/Classes/ClassSearchViewController.m
+++ ClassBrowser/Classes/ClassSearchViewController.m
@@ -4,52 +4,64 @@
 //
 
 #import <objc/runtime.h>
-#import "ClassBrowserAppDelegate.h"
 #import "ClassSearchViewController.h"
+#import "ClassBrowserAppDelegate.h"
 #import "ClassTree.h"
 #import "SubclassesDataSource.h"
 #import "SubclassesWithImageSectionsDataSource.h"
 
-
 @implementation ClassSearchViewController
-
 
 @synthesize tableView;
 @synthesize segmentedControl;
+@synthesize searchBar;
 @synthesize tabBar;
-@synthesize dataSourcesArray = dataSourcesArray_;
-@synthesize initialDataSourcesArray = initialDataSourcesArray_;
+@synthesize dataSourcesArray;
+@synthesize initialDataSourcesArray;
+@synthesize previousSearchText;
 
 
 - (void)dealloc {
 	[tableView release];
 	[segmentedControl release];
+	[searchBar release];
 	[tabBar release];
-	[dataSourcesArray_ release];
-	[initialDataSourcesArray_ release];
+	[dataSourcesArray release];
+	[initialDataSourcesArray release];
+	[previousSearchText release];
     [super dealloc];
 }
+
+
+- (void)loadDataSources {
+	NSUInteger tag = 0;
+	NSMutableArray *array = [[NSMutableArray alloc] initWithCapacity:2];
+	self.dataSourcesArray = array;
+	[array release];
+	array = [[NSMutableArray alloc] initWithCapacity:2];
+	self.initialDataSourcesArray = array;
+	[array release];
+	
+	[dataSourcesArray addObject:[ClassTree sharedClassTree].subclassesDataSource];
+	[initialDataSourcesArray addObject:[ClassTree sharedClassTree].subclassesDataSource];
+	[[tabBar.items objectAtIndex:[dataSourcesArray count] - 1] setTag:tag++];
+	
+	[dataSourcesArray addObject:[ClassTree sharedClassTree].subclassesWithImageSectionsDataSource];
+	[initialDataSourcesArray addObject:[ClassTree sharedClassTree].subclassesWithImageSectionsDataSource];
+	[[tabBar.items objectAtIndex:[dataSourcesArray count] - 1] setTag:tag++];
+	
+	tabBar.selectedItem = [tabBar.items objectAtIndex:0];
+	tableView.dataSource = [dataSourcesArray objectAtIndex:tabBar.selectedItem.tag];
+	[tableView reloadData];
+}	
 
 
 #pragma mark UIViewController Class
 
 
 - (void)viewDidLoad {
-	NSUInteger tag = 0;
-	dataSourcesArray_ = [[NSMutableArray alloc] initWithCapacity:2];
-	initialDataSourcesArray_ = [[NSMutableArray alloc] initWithCapacity:2];
-	
-	[self.dataSourcesArray addObject:[ClassTree sharedClassTree].subclassesDataSource];
-	[self.initialDataSourcesArray addObject:[ClassTree sharedClassTree].subclassesDataSource];
-	[[[self.tabBar items] objectAtIndex:[self.dataSourcesArray count]-1] setTag:tag++];
-
-	[self.dataSourcesArray addObject:[ClassTree sharedClassTree].subclassesWithImageSectionsDataSource];
-	[self.initialDataSourcesArray addObject:[ClassTree sharedClassTree].subclassesWithImageSectionsDataSource];
-	[[[self.tabBar items] objectAtIndex:[self.dataSourcesArray count]-1] setTag:tag++];
-	
-	self.tabBar.selectedItem = [tabBar.items objectAtIndex:0];
-	tableView.dataSource = [self.dataSourcesArray objectAtIndex:tabBar.selectedItem.tag];
-	[self.tableView reloadData];
+	searchBar.autocorrectionType = UITextAutocorrectionTypeNo;
+	[self loadDataSources];
 }
 
 
@@ -71,77 +83,28 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 	if (segmentedControl.selectedSegmentIndex == 0) {
 		ClassBrowserAppDelegate *appDelegate = (ClassBrowserAppDelegate *)[[UIApplication sharedApplication] delegate];
-		[appDelegate pushClass:[[(IndexedDataSource*)self.tableView.dataSource objectForRowAtIndexPath:indexPath] description]];
+		[appDelegate pushClass:[[[dataSourcesArray objectAtIndex:tabBar.selectedItem.tag] objectForRowAtIndexPath:indexPath] description]];
 	} else if (segmentedControl.selectedSegmentIndex == 1) {
-		NSString *className = [(IndexedDataSource*)self.tableView.dataSource objectForRowAtIndexPath:indexPath];
-		NSMutableArray *tree = [[NSMutableArray alloc] initWithObjects:className,nil];
-		NSString *superClassName = [NSString stringWithCString:class_getName(class_getSuperclass(objc_getClass([className cStringUsingEncoding:NSNEXTSTEPStringEncoding]))) encoding:NSNEXTSTEPStringEncoding];
-		while (![superClassName isEqualToString:@"nil"]) {
-			[tree addObject:superClassName];
-			superClassName = [NSString stringWithCString:class_getName(class_getSuperclass(objc_getClass([superClassName cStringUsingEncoding:NSNEXTSTEPStringEncoding]))) encoding:NSNEXTSTEPStringEncoding];
+		NSString *className = [[[dataSourcesArray objectAtIndex:tabBar.selectedItem.tag] objectForRowAtIndexPath:indexPath] description];
+		NSMutableArray *classNameArray = [[NSMutableArray alloc] initWithObjects:className,nil];
+		const char *superClassName = class_getName(class_getSuperclass(objc_getClass([className cStringUsingEncoding:NSNEXTSTEPStringEncoding])));
+		/*
+		 Based on "Objective-C 2.0 Runtime Reference", superClassName become empty string when superClassName reached rootclasses.
+		 So condition will,
+		 > while (strlen(superClassName)) {
+		 But current class_getName return "nil"
+		 */
+		while (strcmp(superClassName,"nil")) {
+			className = [[NSString alloc] initWithCString:superClassName encoding:NSNEXTSTEPStringEncoding];
+			[classNameArray addObject:className];
+			[className release];
+			superClassName = class_getName(class_getSuperclass(objc_getClass(superClassName)));
 		}
 		ClassBrowserAppDelegate *appDelegate = (ClassBrowserAppDelegate *)[[UIApplication sharedApplication] delegate];
-		[appDelegate performSelector:@selector(pushClassTree:) withObject:tree afterDelay:0.1];
-		[tree release];
+		appDelegate.autoPushClassNames = classNameArray;
+		[classNameArray release];
+		[self.navigationController popToRootViewControllerAnimated:YES];
 	}
-}
-
-
-#pragma mark UISearchBarDelegate
-
-
-- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
-	if (searchBar.text.length > 0) {
-		NSArray *classNamesArray = [[[ClassTree sharedClassTree].classDictionary allKeys] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
-		NSMutableArray *filteredArray = [NSMutableArray arrayWithCapacity:[classNamesArray count]];
-		for (NSString *className in classNamesArray) {
-			NSComparisonResult result = [className compare:searchText options:NSCaseInsensitiveSearch range:NSMakeRange(0, [searchText length])];
-			if (result == NSOrderedSame) {
-				[filteredArray addObject:className];
-			}
-		}
-		SubclassesDataSource * subclassesDataSource = [[SubclassesDataSource alloc] initWithArray:filteredArray];
-		[self.dataSourcesArray replaceObjectAtIndex:0 withObject:subclassesDataSource];
-		[subclassesDataSource release];
-		SubclassesWithImageSectionsDataSource *subclassesWithImageSectionsDataSource = [[SubclassesWithImageSectionsDataSource alloc] initWithArray:filteredArray];
-		[self.dataSourcesArray replaceObjectAtIndex:1 withObject:subclassesWithImageSectionsDataSource];
-		[subclassesWithImageSectionsDataSource release];
-	} else {
-		[self.dataSourcesArray replaceObjectAtIndex:0 withObject:[self.initialDataSourcesArray objectAtIndex:0]];
-		[self.dataSourcesArray replaceObjectAtIndex:1 withObject:[self.initialDataSourcesArray objectAtIndex:1]];
-	}
-	
-	tableView.dataSource = [self.dataSourcesArray objectAtIndex:tabBar.selectedItem.tag];
-	[self.tableView reloadData];
-}
-
-
-- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
-	if (searchBar.text.length > 0) {
-		[self.dataSourcesArray replaceObjectAtIndex:0 withObject:[self.initialDataSourcesArray objectAtIndex:0]];
-		[self.dataSourcesArray replaceObjectAtIndex:1 withObject:[self.initialDataSourcesArray objectAtIndex:1]];
-		self.tableView.dataSource = [self.initialDataSourcesArray objectAtIndex:tabBar.selectedItem.tag];
-	}
-	
-	[tableView reloadData];
-	
-	[searchBar resignFirstResponder];
-	searchBar.text = @"";
-}
-
-
-- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
-	[searchBar resignFirstResponder];
-}
-
-
-- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
-	searchBar.showsCancelButton = YES;
-}
-
-
-- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
-	searchBar.showsCancelButton = NO;
 }
 
 
@@ -149,8 +112,76 @@
 
 
 - (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item {
-	tableView.dataSource = [self.dataSourcesArray objectAtIndex:item.tag];
-	[self.tableView reloadData];
+	tableView.dataSource = [dataSourcesArray objectAtIndex:item.tag];
+	[tableView reloadData];
+}
+
+
+#pragma mark UISearchBarDelegate
+
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+	if (searchText.length > 0) {
+		// from subclassesDataSource
+		SubclassesDataSource *currentSubclassesDataSource;
+		if (previousSearchText && [searchText hasPrefix:previousSearchText]) {
+			currentSubclassesDataSource = [dataSourcesArray objectAtIndex:0];
+		} else {
+			currentSubclassesDataSource = [initialDataSourcesArray objectAtIndex:0];
+		}
+		self.previousSearchText = searchText;
+		NSArray *classNamesArray = [[currentSubclassesDataSource.rows objectForKey:[searchText capitalChar]] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+		NSMutableArray *filteredArray = [NSMutableArray arrayWithCapacity:[classNamesArray count]];
+		for (NSString *className in classNamesArray) {
+			NSComparisonResult result = [className compare:searchText options:NSCaseInsensitiveSearch range:NSMakeRange(0, searchText.length)];
+			if (result == NSOrderedSame) {
+				[filteredArray addObject:className];
+			} else if (result == NSOrderedDescending) {
+				break;
+			}
+		}
+		SubclassesDataSource * subclassesDataSource = [[SubclassesDataSource alloc] initWithArray:filteredArray];
+		[dataSourcesArray replaceObjectAtIndex:0 withObject:subclassesDataSource];
+		[subclassesDataSource release];
+		SubclassesWithImageSectionsDataSource *subclassesWithImageSectionsDataSource = [[SubclassesWithImageSectionsDataSource alloc] initWithArray:filteredArray];
+		[dataSourcesArray replaceObjectAtIndex:1 withObject:subclassesWithImageSectionsDataSource];
+		[subclassesWithImageSectionsDataSource release];
+	} else {
+		[dataSourcesArray replaceObjectAtIndex:0 withObject:[initialDataSourcesArray objectAtIndex:0]];
+		[dataSourcesArray replaceObjectAtIndex:1 withObject:[initialDataSourcesArray objectAtIndex:1]];
+	}
+	
+	tableView.dataSource = [dataSourcesArray objectAtIndex:tabBar.selectedItem.tag];
+	[tableView reloadData];
+}
+
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+	if (self.searchBar.text.length > 0) {
+		[dataSourcesArray replaceObjectAtIndex:0 withObject:[initialDataSourcesArray objectAtIndex:0]];
+		[dataSourcesArray replaceObjectAtIndex:1 withObject:[initialDataSourcesArray objectAtIndex:1]];
+		tableView.dataSource = [initialDataSourcesArray objectAtIndex:tabBar.selectedItem.tag];
+	}
+	
+	[tableView reloadData];
+	
+	[self.searchBar resignFirstResponder];
+	self.searchBar.text = @"";
+}
+
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+	[self.searchBar resignFirstResponder];
+}
+
+
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+	self.searchBar.showsCancelButton = YES;
+}
+
+
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+	self.searchBar.showsCancelButton = NO;
 }
 
 
