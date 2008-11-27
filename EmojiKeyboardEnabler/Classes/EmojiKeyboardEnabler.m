4e2c62f0b3de96ad951650fedce7af008dd2bea8
野村 憲男 <norio.nomura@gmail.com>
2008-11-27 12:55:48 +0900

    enable Emoji when user selected keyboard is one

--- EmojiKeyboardEnabler/Classes/EmojiKeyboardEnabler.m
+++ EmojiKeyboardEnabler/Classes/EmojiKeyboardEnabler.m
@@ -5,36 +5,52 @@
 #import <objc/runtime.h>
 #import "EmojiKeyboardEnabler.h"
 
-@implementation NSObject(EmojiHack)
+@implementation UIView(EmojiKeyboardEnabler)
+
+static NSString *triggerInputMode = nil;
 
 
-- (id)__setInputMode:(NSString*)obj {
+- (id)__setInputMode:(NSString*)mode {
 	static BOOL toggle = YES;
 	id result;
-	if (toggle && [obj isEqual:@"ja_JP-Romaji"]) {
+	if (toggle && [mode isEqual:triggerInputMode]) {
 		result = [self __setInputMode:@"emoji"];
 		toggle = NO;
 	} else {
-		result = [self __setInputMode:obj];
+		result = [self __setInputMode:mode];
 		toggle = YES;
 	}
 	return result;
 }
 
 
-@end
+- (id)__inputModePreference {
+	NSMutableArray *array;
+	array = [self __inputModePreference];
+	if (!triggerInputMode) {
+		triggerInputMode = [array objectAtIndex:0];
+	}
+	if (NSNotFound == [array indexOfObject:@"emoji"]) {
+		[array addObject:@"emoji"];
+	}
+	return array;
+}
 
+
+@end
 
 static BOOL installedEmojiKeyboardEnabler = NO;
 
 
-BOOL installEmojiKeyboardEnabler() {
-	if (!installedEmojiKeyboardEnabler) {
+BOOL EmojiKeyboardEnable(BOOL bEnable) {
+	if (installedEmojiKeyboardEnabler != bEnable) {
 		Class class = objc_getClass("UIKeyboardImpl");
 		if (class) {
 			method_exchangeImplementations(class_getInstanceMethod(class, @selector(setInputMode:)),
 										   class_getInstanceMethod(class, @selector(__setInputMode:)));
-			installedEmojiKeyboardEnabler = YES;
+			method_exchangeImplementations(class_getInstanceMethod(class, @selector(inputModePreference)),
+										   class_getInstanceMethod(class, @selector(__inputModePreference)));
+			installedEmojiKeyboardEnabler = bEnable;
 			return YES;
 		}
 	}
@@ -42,15 +58,11 @@ BOOL installEmojiKeyboardEnabler() {
 }
 
 
+BOOL installEmojiKeyboardEnabler() {
+	return EmojiKeyboardEnable(YES);
+}
+
+
 BOOL uninstallEmojiKeyboardEnabler() {
-	if (installedEmojiKeyboardEnabler) {
-		Class class = objc_getClass("UIKeyboardImpl");
-		if (class) {
-			method_exchangeImplementations(class_getInstanceMethod(class, @selector(setInputMode:)),
-										   class_getInstanceMethod(class, @selector(__setInputMode:)));
-			installedEmojiKeyboardEnabler = NO;
-			return YES;
-		}
-	}
-	return NO;
+	return EmojiKeyboardEnable(NO);
 }
